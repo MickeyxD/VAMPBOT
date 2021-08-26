05bdfaa67aed031835be5f8a60bcd1a11ec1cf61
MICKEY♡ <88245294+D15H4NT0P@users.noreply.github.com>
2021-08-27 00:26:21 +0530

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#   Copyright (C) 2021 W2HGalaxy-OP
+#   Copyright (C) 2021 D15H4NT0P
 #
 #   Poject VΛDΣЯ is free software: 
 #   you can redistribute it and/or modify it under the terms of the GNU General Public License 
